cc37278e57f3ec670340dcd072b77c0252538412
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-25 02:15:40 +0900

    Update hello.erl

--- hello.erl
+++ hello.erl
@@ -1,6 +1,21 @@
+
+##############################
+[AC]
+
+-module('Main').
+-export([main/1]).
+main(_) ->
+  io:format("Hello, world!~n"),
+  halt().
+  
+
+##############################
+[WA]
+
 -module('Main').
 -export([main/1]).
 main(_) ->
   io:format("Hello, world!"),
   halt().
   
+##############################

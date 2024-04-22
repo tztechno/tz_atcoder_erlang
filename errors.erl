4b13565cedc30eb843494543509ac7c819809b3b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-04-22 14:48:18 +0900

    Update errors.erl

--- errors.erl
+++ errors.erl
@@ -3,11 +3,29 @@
 #####################################
 #####################################
 #####################################
-#####################################
-
 Runtime terminating during boot ({{badmatch,{error,terminated}},[{Main,main,1,[{_},{_}]},{init,start_em,1,[]},{init,do_boot,3,[]}]})
 
 Crash dump is being written to: erl_crash.dump...done
 
+-------------------------------------
+
+-module('Main').
+-export([main/1]).
+
+main(_) ->
+  {ok, [S]} = io:fread("", "~s"),
+  io:format("~s", [S]),
+  halt().
+
+#####################################
+
+Runtime terminating during boot 
+
+({{badmatch,{error,terminated}},
+[{Main,main,1,[{_},{_}]},
+{init,start_em,1,[]},
+{init,do_boot,3,[]}]})
+
+Crash dump is being written to: erl_crash.dump...done
 
 #####################################

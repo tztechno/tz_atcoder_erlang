c51e4ddf3628390b8406303d8146456568da10d5
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-01 12:15:52 +0900

    Update errors.erl

--- errors.erl
+++ errors.erl
@@ -3,11 +3,11 @@
 #####################################
 #####################################
 #####################################
-Runtime terminating during boot ({{badmatch,{error,terminated}},[{Main,main,1,[{_},{_}]},{init,start_em,1,[]},{init,do_boot,3,[]}]})
 
+Runtime terminating during boot ({{badmatch,{error,terminated}},[{Main,main,1,[{_},{_}]},{init,start_em,1,[]},{init,do_boot,3,[]}]})
 Crash dump is being written to: erl_crash.dump...done
 
--------------------------------------
+#####################################
 
 -module('Main').
 -export([main/1]).

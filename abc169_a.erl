79dff7a08e5c37a4a33d46dd03ae4594fd0cee5f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-01 12:13:39 +0900

    Update abc169_a.erl

--- abc169_a.erl
+++ abc169_a.erl
@@ -1,5 +1,9 @@
 [今, error]
-
+--------------------------------------------------------------------
+Runtime terminating during boot ({{badmatch,{error,terminated}},
+[{Main,main,1,[{_},{_}]},{init,start_em,1,[]},{init,do_boot,3,[]}]})
+Crash dump is being written to: erl_crash.dump...done
+--------------------------------------------------------------------
 -module('Main').
 -export([main/1]).
 
@@ -7,3 +11,4 @@ main(_) ->
     {ok, [B, C]} = io:fread("", "~d~d"),
     io:format("~p ", [B * C]),
     halt().
+--------------------------------------------------------------------
